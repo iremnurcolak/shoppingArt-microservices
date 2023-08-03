21a5c07414514c18ada4114fbfefca7d6c64b043
iremnurcolak <iremnurcolak34@gmail.com>
2023-08-03 12:35:08 +0300

    Allowed inventory service to create multiple instances, added loadbalanced feature to order service's webclientconfig to handle multiple instances of inventory service while requesting to it

--- shoppingArt-orderService/src/main/java/com/lobemus/shoppingApporderService/service/OrderService.java
+++ shoppingArt-orderService/src/main/java/com/lobemus/shoppingApporderService/service/OrderService.java
@@ -23,7 +23,7 @@ public class OrderService {
     private final OrderRepository orderRepository;
 
     //var name should be same with the bean method name
-    private final WebClient webClient;
+    private final WebClient.Builder webClientBuilder;
     public void placeOrder(OrderRequest orderRequest)
     {
         Order order = new Order();
@@ -44,8 +44,8 @@ public class OrderService {
 
         //Call Inventory service, and place order if product is in stock
         //SYNC Communication
-        InventoryResponse[] inventoryResponses = webClient.get()
-                .uri("http://localhost:8082/api/inventory",
+        InventoryResponse[] inventoryResponses = webClientBuilder.build().get()
+                .uri("http://inventory-service/api/inventory",
                         uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
                         .retrieve()
                                 .bodyToMono(InventoryResponse[].class)
