f88892ca05fdb6d3d200ab39f52db2e72410f24f
iremnurcolak <iremnurcolak34@gmail.com>
2023-08-05 13:27:37 +0300

    Updated security web filter chain

--- shoppingArt-apiGateway/src/main/java/config/SecurityConfig.java
+++ shoppingArt-apiGateway/src/main/java/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
@@ -13,13 +14,15 @@ public class SecurityConfig {
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity serverHttpSecurity) {
         serverHttpSecurity
-                //we don't need authorization for loading eureka's css and other static sources
-                .authorizeExchange(exchange -> exchange.pathMatchers("/eureka/**")
-                        .permitAll()
-                        .anyExchange()
-                        .authenticated())
-                .oauth2ResourceServer((oauth2) -> oauth2
-                .jwt(Customizer.withDefaults()));
+                .csrf(csrfSpec -> csrfSpec.disable())
+                .authorizeExchange(exchange ->
+                        exchange.pathMatchers("/eureka/**")
+                                .permitAll()
+                                .anyExchange()
+                                .authenticated())
+                .oauth2ResourceServer(oauth2ResourceServer ->
+                        oauth2ResourceServer.jwt(Customizer.withDefaults()));
+
         return serverHttpSecurity.build();
     }
 }
