d6658ef62242253a53d20f6d76d78aac1fc59ccc
iremnurcolak <iremnurcolak34@gmail.com>
2023-08-05 14:23:57 +0300

    Deleted unnecessary import

--- shoppingArt-apiGateway/src/main/java/config/SecurityConfig.java
+++ shoppingArt-apiGateway/src/main/java/config/SecurityConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
