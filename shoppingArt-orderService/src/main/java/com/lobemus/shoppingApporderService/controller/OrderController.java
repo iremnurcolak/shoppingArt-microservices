a5cb488299e2feaa27218d6c6f1ed9aef98f2bc4
iremnurcolak <iremnurcolak34@gmail.com>
2023-08-08 00:25:26 +0300

    Implemented retry case circuit breaker while ordering place

--- shoppingArt-orderService/src/main/java/com/lobemus/shoppingApporderService/controller/OrderController.java
+++ shoppingArt-orderService/src/main/java/com/lobemus/shoppingApporderService/controller/OrderController.java
@@ -3,6 +3,7 @@ package com.lobemus.shoppingApporderService.controller;
 import com.lobemus.shoppingApporderService.dto.OrderRequest;
 import com.lobemus.shoppingApporderService.service.OrderService;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -21,6 +22,7 @@ public class OrderController {
     @ResponseStatus(HttpStatus.CREATED)
     @CircuitBreaker(name = "inventory", fallbackMethod = "placeOrderFallback")
     @TimeLimiter(name = "inventory")
+    @Retry(name = "inventory")
     public CompletableFuture<String> placeOrder(@RequestBody OrderRequest orderRequest)
     {
         return CompletableFuture.supplyAsync(() -> orderService.placeOrder(orderRequest));
